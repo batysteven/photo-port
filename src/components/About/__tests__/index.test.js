a18a584280872f2c6f02928c04c0f82f17cd77cd
Steven Baty <baty_steven@yahoo.com>
2021-04-21 00:55:44 -0600

    add test code for about and passed

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -10,6 +10,11 @@ describe('About component', () => {
     });
 
     // Second Test
+    it('matches snapshot DOM node structure', () => {
+        // render About
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    });
 })
 
 afterEach(cleanup);
